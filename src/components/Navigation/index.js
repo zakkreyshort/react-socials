143783bddaac243f63aa4b198df9cd9006dfef0c
Zakkrey Short <zakkreyjshort@gmail.com>
2020-08-31 10:47:25 -0700

    successfully added new user to db

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -30,11 +30,11 @@ const NavigationAuth = ({ authUser }) => (
     <li>
       <Link to={ROUTES.ACCOUNT}>Account</Link>
     </li>
-    {authUser.roles.includes(ROLES.ADMIN) && (
+    {/* {authUser.roles.includes(ROLES.ADMIN) && (
       <li>
         <Link to={ROUTES.ADMIN}>Admin</Link>
       </li>
-    )}
+    )} */}
     <li>
       <SignOutButton />
     </li>
